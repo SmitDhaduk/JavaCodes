883c6d0b5b379f47ceadbedab153d8003cc29ae9
SmitDhaduk <110409162+SmitDhaduk@users.noreply.github.com>
2022-08-01 11:28:36 -0400

    Add files via upload
    In this code, I have checked if the number is positive, negative or zero.

---
+++ functions.java
@@ -0,0 +1,27 @@
+import java.sql.Date;
+import java.util.Scanner;
+
+public class functions {
+
+	public static void main(String[] args) {
+	
+		Scanner sc  = new Scanner(System.in);
+		
+		System.out.println("Enter any number: ");
+		
+		int num = sc.nextInt();
+		if(num>0) {
+			System.out.println("Entered number is positive.");
+		}else if(num==0){
+			System.out.println("Entered number is zero.");
+			
+		}else {
+			System.out.println("Enterd number is negative.");
+		}
+		
+		
+		
+		
+	}
+	
+}
