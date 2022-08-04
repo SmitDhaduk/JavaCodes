bedc6811b98cc99fcbd28817543bb6af2a823212
Smit Dhaduk <110409162+SmitDhaduk@users.noreply.github.com>
2022-08-04 16:34:49 -0400

    Add files via upload

---
+++ ForEach.java
@@ -0,0 +1,9 @@
+public class ForEach {
+    public static void main(String[] args){
+        String[] fruits = {"Apple","Orange","Pineapple"};
+        for(String fruit : fruits){
+            System.out.println(fruit);
+        }
+    }
+    
+}
