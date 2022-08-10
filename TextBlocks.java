7da81d3ece65a5c7bf1929ed41d1d3ee219b6555
Smit Dhaduk <110409162+SmitDhaduk@users.noreply.github.com>
2022-08-10 03:03:53 -0400

    Add files via upload

---
+++ TextBlocks.java
@@ -0,0 +1,13 @@
+public class TextBlocks {
+
+    // In this code i have write text block.
+    // which use to store newlines and quotes without the need for escaping line breaks.
+    public static void main(String[] args) {
+        String text = """
+                Hi,
+                I am Smit.
+                """;
+                System.out.println(text);
+    }
+    
+}
